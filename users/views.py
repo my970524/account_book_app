6d67dda2cd2db4f93d425b8df91976419c93f372
minji <my970524@naver.com>
2022-08-22 01:58:24 +0900

    Feat : 유저 관리 api 개발
    - 회원가입 기능 개발
    - SignUpSerializer 생성
    - SignUpView 생성
    - users/urls.py 생성(url 맵핑)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-# from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import SignUpSerializer
+
+
+#  url : POST /api/users/signup
+class SignUpView(APIView):
+    """
+    Assignee : 민지
+
+    회원가입 view 입니다.
+    """
+
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "회원가입이 성공적으로 되었습니다."}, status=status.HTTP_201_CREATED)
+        return Response({"message": "회원가입에 실패했습니다."}, status=status.HTTP_400_BAD_REQUEST)
