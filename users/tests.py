c30cc8510d9e42a0923f8cac10ea68d5582da78d
minji <my970524@naver.com>
2022-08-22 02:19:08 +0900

    Add : #4 유저 관리 api 개발
    - 로그인 기능 테스트케이스 작성

--- users/tests.py
+++ users/tests.py
@@ -42,3 +42,52 @@ class SignUpTest(TestCase):
         }
         response = client.post(self.url, sign_up_info, format="json")
         self.assertEqual(response.status_code, 400)
+
+
+class SignInViewTest(TestCase):
+    """
+    Assignee : 민지
+
+    로그인 기능을 테스트 합니다.
+    """
+
+    url = "/api/users/signin"
+
+    def setUp(self):
+        self.email = "test1@gmail.com"
+        self.username = "test1"
+        self.password = "test1"
+        self.user_test1 = User.objects.create_user(self.email, self.username, self.password)
+
+    def test_signin_success(self):
+        """로그인 성공을 테스트 합니다."""
+        client = Client()
+        sign_in_info = {
+            "email": "test1@gmail.com",
+            "password": "test1",
+        }
+        response = client.post(self.url, sign_in_info, format="json")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("message" in response.content)
+        self.assertIn("access_token" in response.content)
+        self.assertIn("refresh_token" in response.content)
+
+    def test_signin_fail_with_wrong_email(self):
+        """잘못된 이메일로 로그인 실패를 테스트 합니다."""
+        client = Client()
+        sign_in_info = {
+            "email": "test1@email.com",
+            "password": "test1",
+        }
+        response = client.post(self.url, sign_in_info, format="json")
+        self.assertEqual(response.status_code, 400)
+
+    def test_signin_fail_with_wrong_password(self):
+        """잘못된 비밀번호로 로그인 실패를 테스트 합니다."""
+        client = Client()
+        sign_in_info = {
+            "email": "test1@gmail.com",
+            "password": "test11",
+        }
+        response = client.post(self.url, sign_in_info, format="json")
+        self.assertEqual(response.status_code, 400)
