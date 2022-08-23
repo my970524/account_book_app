87a26290eb4f3819d6c4b1eef7f4c3d8cdfc0c27
minji <my970524@naver.com>
2022-08-24 00:11:23 +0900

    Add : #6 가계부 관련 api 개발
    - 가계부 수정 테스트 케이스 작성

--- account_books/tests.py
+++ account_books/tests.py
@@ -144,3 +144,74 @@ class ListAccountBookTest(TestCase):
         response = client.get(self.url + "?is_deleted=True", format="json", **header)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content.decode().count("title"), 1)
+
+
+class UpdateAccountBookTest(TestCase):
+    """
+    Assignee : 민지
+
+    가계부 수정 기능을 테스트 합니다.
+    관리자와 작성자 본인만 수정 가능합니다.
+    """
+
+    def setUp(self):
+        self.user_test1 = User.objects.create_user(
+            email="test1@gmail.com",
+            username="test1",
+            password="test1",
+        )
+        self.user_test2 = User.objects.create_user(
+            email="test2@gmail.com",
+            username="test2",
+            password="test2",
+        )
+
+        self.test1_account_book1 = AccountBook.objects.create(
+            writer=self.user_test1,
+            title="7월 가계부",
+            balance=200000,
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+        AccountBook.objects.all().delete()
+
+    def test_update_account_books_by_owner(self):
+        """본인의 가계부 수정을 테스트 합니다."""
+
+        client = Client()
+
+        sign_in_info = {
+            "email": "test1@gmail.com",
+            "password": "test1",
+        }
+        sign_in_response = client.post("/api/users/signin", sign_in_info, format="json")
+        header = {"HTTP_AUTHORIZATION": f'Bearer {json.loads(sign_in_response.content)["access_token"]}'}
+
+        account_book_id = AccountBook.objects.get(title="7월 가계부").id
+        url = f"/api/v1/account_books/{account_book_id}"
+
+        new_account_book_data = {"balance": 300000}
+
+        response = self.client.put(url, new_account_book_data, format="json", **header)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(response.content.decode(), 200000)
+
+    def test_update_account_books_by_other(self):
+        """다른 사람의 가계부 수정을 테스트 합니다."""
+
+        client = Client()
+
+        sign_in_info = {
+            "email": "test2@gmail.com",
+            "password": "test2",
+        }
+        sign_in_response = client.post("/api/users/signin", sign_in_info, format="json")
+        header = {"HTTP_AUTHORIZATION": f'Bearer {json.loads(sign_in_response.content)["access_token"]}'}
+
+        account_book_id = AccountBook.objects.get(title="7월 가계부").id
+        url = f"/api/v1/account_books/{account_book_id}"
+
+        new_account_book_data = {"balance": 300000}
+        response = self.client.put(url, new_account_book_data, format="json", **header)
+        self.assertEqual(response.status_code, 401)
