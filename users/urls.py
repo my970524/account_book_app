49338e56df867a5d0c48a66d941c037a7577035d
minji <my970524@naver.com>
2022-08-25 04:49:38 +0900

    Feat : #11 유저관리 기능 추가 개발
    - access 토큰 재발급 기능 추가
    - url 맵핑(/api/users/token/refresh)

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import SignInView, SignUpView
 
 urlpatterns = [
     path("signup", SignUpView.as_view(), name="signup"),
     path("signin", SignInView.as_view(), name="signin"),
+    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
 ]
